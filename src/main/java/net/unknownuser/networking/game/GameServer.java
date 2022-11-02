c9b7eafc13ee0863e65e2718df82f4428acb5dd4
Luca S <luca.saalfeld@volkswagen.de>
2022-11-02 11:02:06 +0100

    basic online functionality; untested

--- src/main/java/net/unknownuser/networking/game/GameServer.java
+++ src/main/java/net/unknownuser/networking/game/GameServer.java
@@ -9,9 +9,12 @@ import net.unknownuser.networking.*;
 
 public class GameServer extends Server {
 	private final HashMap<Connection, Integer> connectionIDs = new HashMap<>();
-//	private final HashMap<Integer, Point> playerPositions = new HashMap<>();
 	private final HashMap<Integer, Tuple<String, RGB>> playerSettings = new HashMap<>();
 	private int idIndex = 0;
+	
+	private static final Point DEFAULT_STARTING_POSITION = new Point(0, 0);
+	
+	private Board board = new Board(20, 15);
 	
 	protected GameServer(int port) {
 		super(port);
@@ -20,6 +23,10 @@ public class GameServer extends Server {
 	public static void main(String[] args) throws IOException {
 		GameServer server = new GameServer(50000);
 		server.start();
+	}
+	
+	public Point getDefaultStartingPoint() {
+		return new Point(DEFAULT_STARTING_POSITION.x, DEFAULT_STARTING_POSITION.y);
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -43,6 +50,11 @@ public class GameServer extends Server {
 				playerSettings.put(senderID, playerPrefs);
 			}
 		}
+		case MOVE -> {
+			MoveDirection dir = (MoveDirection) message.content;
+			board.movePlayer(senderID, dir);
+			broadcastMessage(new MessageToSend(new Message<>(MessageType.MOVE, new Tuple<>(senderID, dir)), sender));
+		}
 		default -> System.out.println("unkown or unhandled message type " + message.type);
 		}
 	}
@@ -51,13 +63,30 @@ public class GameServer extends Server {
 	public void onClientConnected(Connection client) {
 		synchronized (connectionIDs) {
 			connectionIDs.put(client, ++idIndex);
+			Point pos = getDefaultStartingPoint();
+			board.addPlayer(idIndex, pos);
+			// give ID to new player
 			client.sendMessage(new Message<>(MessageType.SET_ID, idIndex));
+			// notify all other players of new player
+			broadcastMessage(new MessageToSend(new Message<>(MessageType.NEW_PLAYER, new Tuple<>(idIndex, pos)), client));
+			
 			System.out.println("client connected");
 		}
 	}
 	
 	@Override
 	public void onClientDisconnected(Connection client) {
+		int senderID = -1;
+		synchronized (connectionIDs) {
+			senderID = connectionIDs.get(client);
+		}
+		
+		broadcastMessage(new MessageToSend(new Message<>(MessageType.DELETE_PLAYER, senderID), client));
+		
+		// remove player from maps
+		playerSettings.remove(senderID);
+		connectionIDs.remove(client);
+		
 		System.out.println("client disconnected");
 	}
 	
